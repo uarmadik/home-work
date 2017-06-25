334902352c837461339660733a0c79787ccefb21
uarmadik <uarmadik@gmail.com>
2017-06-25 11:05:56 +0300

    Додано перевірку вхідних даних, оптимізовано конструкції if else

--- tour/php/count.php
+++ tour/php/count.php
@@ -1,43 +1,25 @@
 <?php
-
     $number_of_days = $_GET['number-of-days'];
-    //$count_tour = $number_of_days * 100;
-
     $country = $_GET['country'];
-
-    if ($_GET['discount']) {
-        if ($country == 'Turkey') {
-            $count_tour = $number_of_days * 100;
-            $count_tour *= 0.95;
-            echo "Вартість відпочинку у $country становить $count_tour USD";
+    $cost_day = 100;
+    $discount = 0.05;
+    $cost_tour = "temp";
+    if (is_numeric($number_of_days) && ($number_of_days > 0) && $country) {
+        if ($country == 'turkey') {
+            $cost_tour = $cost_day * $number_of_days;
         }
-        else if ($country == 'Egypt') {
-            $count_tour = 1.1 * $number_of_days * 100;
-            $count_tour *= 0.95;
-            echo "Вартість відпочинку у $country становить $count_tour USD";
+        elseif ($country == 'egypt') {
+            $cost_tour = $cost_day * $number_of_days * 1.1;
         }
-        else if ($country == 'Italy') {
-            $count_tour = 1.2 * $number_of_days * 100;
-            $count_tour *= 0.95;
-            echo "Вартість відпочинку у $country становить $count_tour USD";
-        }
-        else {
-            echo "Помилка!";
+        elseif ($country == 'italy') {
+            $cost_tour = $cost_day * $number_of_days * 1.2;
         }
     }
     else {
-        if ($country == 'Turkey') {
-            $count_tour = $number_of_days * 100;
-            echo "Вартість відпочинку у $country становить $count_tour USD";
-        }
-        else if ($country == 'Egypt') {
-            $count_tour = 1.1 * $number_of_days * 100;
-            echo "Вартість відпочинку у $country становить $count_tour USD";
-        }
-        else if ($country == 'Italy') {
-            $count_tour = 1.2 * $number_of_days * 100;
-            echo "Вартість відпочинку у $country становить $count_tour USD";
-        }
-
+        echo "Помилка! Всі поля повинні бути вибрані.";
+        return;
     }
-
+    if ($_GET['discount']) {
+        $cost_tour *= 1 - $discount;
+    }
+    echo "Вартість даного туру становить $cost_tour USD";
