a6a491a8b9874355620f12d08b4bd03a99a0de8f
Ihor <uarmadik@gmail.com>
2017-08-21 17:29:29 +0300

    test

---
+++ test/Animal.php
@@ -0,0 +1,38 @@
+/*
+  Чтобы Вас пригласили на собеседование, вместе с резюме отправьте решение тестового задания:
+
+  Написать класс Cat, который наследуется от класcа Animal. Класс Animal имеет метод getName (name можно передать в конструктор). Класс Cat имеет метод meow (возвращает строку «Cat {catname} is sayig meow».
+
+  Пример использования:
+
+  $cat = new Cat ('garfield');
+
+  $cat->getName () === 'garfield' // true;
+
+  $cat->meow () === 'Cat garfield is saying meow' // true;
+*/
+
+class Animal
+{
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+class Cat extends Animal
+{
+    protected $name = '';
+    public function meow()
+    {
+        return 'Cat <b>' . $this->name . '</b> is saying meow';
+    }
+    public function __construct($name)
+    {
+        $this->name = ucfirst($name);
+    }
+}
+
+$cat = new Cat("garfild");
+echo $cat->getName();
+    echo '<br />';
+echo $cat->meow();
